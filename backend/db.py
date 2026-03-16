7ba3397588782c5ec783104351f178cbe4c49750
TariqTalhami <tariq.talhami@gmail.com>
2026-03-16 10:35:21 +0200

    modified db.py

--- backend/db.py
+++ backend/db.py
@@ -6,7 +6,8 @@ load_dotenv()   #loads variables from .env file into Python
 
 def get_db_connection():
     database_url = os.getenv("DATABASE_URL")
-
+    if not database_url:
+        raise ValueError("DATABASE_URL environment variable is not set")
     conn = psycopg2.connect(database_url)
 
     return conn
